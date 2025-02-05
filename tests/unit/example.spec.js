011a5434c432a22c2fb36d11dafa23c0edd7fa3c
Begoña Repiso <95415289+larepi@users.noreply.github.com>
2025-02-05 19:42:41 +0100

    Update example.spec.js

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,3 +1,38 @@
+import Rox from 'rox-browser';
+
+const flags = {
+  enableTutorial: new Rox.Flag(),
+  titleColors: new Rox.RoxString('White', ['White', 'Blue', 'Green', 'Yellow']),
+  titleSize: new Rox.RoxNumber(12, [14, 18, 24])
+};
+
+async function initPlatform() {
+  const options = {  };
+
+  // Register the flags
+  Rox.register('', flags);
+
+  // Setup connection with the feature management environment key
+  await Rox.setup('2bb4c014-186c-44c9-8ed0-452601698214', options);
+
+  // Boolean flag example
+  if (flags.enableTutorial.isEnabled()) {
+    console.log('enableTutorial flag is true');
+    // TODO:  Put your code here that needs to be gated
+  }
+
+   // RoxString flag example
+  console.log('Title color is ' + flags.titleColors.getValue());
+
+  // RoxNumber flag example
+  console.log('Title size is ' + flags.titleSize.getValue());
+}
+
+initPlatform().then(function() {
+  console.log('Done loading CloudBees platform');
+});
+
+
 import { shallowMount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
 
